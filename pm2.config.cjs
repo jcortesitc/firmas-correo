db3b07528ecff42f15176e796d74a1bbc3911c1c
JoseITConsultants <jcontreras@itconsultants.cl>
2024-06-28 18:12:03 +0200

    incorporacion de config pm2

---
+++ pm2.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+
+    script: "serve",
+    env: {
+        PM2_SERVE_PATH: './dist',
+        PM2_SERVE_PORT: 8050,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html'
+    }
+
+}
